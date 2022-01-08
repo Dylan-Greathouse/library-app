f5f8929e7561174a126ff511f19ec22907b9d48b
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2022-01-07 16:17:49 -0800

    fixed latte sabatoge

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 import './Footer.css';
 
 function Footer() {
-  return <footer>&copy;2021 &ndash; Alchemy Code Lab</footer>;wwwwa/
+  return <footer>&copy;2021 &ndash; Alchemy Code Lab</footer>;
 }
 
 export default Footer;
